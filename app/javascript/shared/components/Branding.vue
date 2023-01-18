08e0906730859a82b9e107da823d957eae6bfcdc
James Ng <james@freec.asia>
2023-01-18 10:54:23 +0700

    remove branding

--- app/javascript/shared/components/Branding.vue
+++ app/javascript/shared/components/Branding.vue
@@ -1,19 +1,12 @@
 <template>
   <div v-if="globalConfig.brandName" class="px-0 py-3 flex justify-center">
     <a
-      :href="brandRedirectURL"
+      :href="https://freec.asia"
       rel="noreferrer noopener nofollow"
       target="_blank"
       class="branding--link justify-center"
     >
-      <img
-        class="branding--image"
-        :alt="globalConfig.brandName"
-        :src="globalConfig.logoThumbnail"
-      />
-      <span>
-        {{ useInstallationName($t('POWERED_BY'), globalConfig.brandName) }}
-      </span>
+   
     </a>
   </div>
   <div v-else class="p-3" />
